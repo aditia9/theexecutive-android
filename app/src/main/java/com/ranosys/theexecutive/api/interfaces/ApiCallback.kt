1c024d38b323673518c52b074c97ec27cb62c76d
Mohammad Sunny <mohammad.sunny@ranosys.com>
2018-02-26 12:47:34 +0530

    feedback in progress

---
+++ app/src/main/java/com/ranosys/theexecutive/api/interfaces/ApiCallback.kt
@@ -0,0 +1,10 @@
+package com.ranosys.theexecutive.api.interfaces
+
+/**
+ * Created by Vikash Kumar Bijarniya on 29/1/18.
+ */
+interface ApiCallback<T>{
+    fun onException(error : Throwable)
+    fun onError(errorMsg : String)
+    fun onSuccess(t : T)
+}
