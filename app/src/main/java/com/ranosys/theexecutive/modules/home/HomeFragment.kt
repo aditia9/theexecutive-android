9239ad85b88f2ba2eb493c81dd9319165d1c9954
Mohammad Sunny <mohammad.sunny@ranosys.com>
2018-04-12 17:31:11 +0530

    code optimized

--- app/src/main/java/com/ranosys/theexecutive/modules/home/HomeFragment.kt
+++ app/src/main/java/com/ranosys/theexecutive/modules/home/HomeFragment.kt
@@ -34,6 +34,7 @@ class HomeFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setPagerAdapter()
+
     }
 
     private fun setPagerAdapter(){
