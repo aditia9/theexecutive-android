836e4630bf8f61eda8b79b3c85173f173c29d31b
rashpal_singh25 <rashpal.singh@ranosys.com>
2018-05-07 12:45:16 +0530

    Attend Ashok sir feedback

--- app/src/main/java/com/ranosys/theexecutive/modules/changePassword/ChangePasswordViewModel.kt
+++ app/src/main/java/com/ranosys/theexecutive/modules/changePassword/ChangePasswordViewModel.kt
@@ -5,7 +5,6 @@ import android.arch.lifecycle.MutableLiveData
 import android.content.Context
 import android.databinding.ObservableField
 import android.support.design.widget.TextInputEditText
-import android.text.TextUtils
 import com.ranosys.theexecutive.R
 import com.ranosys.theexecutive.api.AppRepository
 import com.ranosys.theexecutive.api.interfaces.ApiCallback
@@ -58,44 +57,28 @@ class ChangePasswordViewModel(application: Application) : BaseViewModel(applicat
 
         var isValid = true
 
-        if ((newPassword.get() == confirmPassword.get()).not()) {
-            confirmPasswordError.set(context.getString(R.string.confirm_password_error))
-            isValid = false
-        }
-
-        if (TextUtils.isEmpty(currentPassword.get())) {
+        if (currentPassword.get().isNullOrBlank()) {
             currentPasswordError.set(context.getString(R.string.empty_current_password))
             isValid = false
         }
 
-        if (TextUtils.isEmpty(newPassword.get())) {
+        if (newPassword.get().isNullOrBlank()) {
             newPasswordError.set(context.getString(R.string.empty_new_password))
-            isValid = false
-        } else if (TextUtils.isEmpty(confirmPassword.get())) {
-            confirmPasswordError.set(context.getString(R.string.empty_confirm_password))
             isValid = false
         }
 
-        if (!TextUtils.isEmpty(newPassword.get()) && !Utils.isValidPassword(newPassword.get())) {
+        if (newPassword.get().isNullOrBlank().not() && !Utils.isValidPassword(newPassword.get())) {
             newPasswordError.set(context.getString(R.string.valid_password_error))
             isValid = false
-        } else if (!TextUtils.isEmpty(confirmPassword.get()) && !Utils.isValidPassword(confirmPassword.get())) {
-            confirmPasswordError.set(context.getString(R.string.valid_password_error))
+        } else if ((newPassword.get().isNullOrBlank().not() &&  confirmPassword.get().isNullOrBlank())) {
+            confirmPasswordError.set(context.getString(R.string.empty_confirm_password))
             isValid = false
-        } else if (!TextUtils.isEmpty(confirmPassword.get()) && !TextUtils.isEmpty(newPassword.get())) {
+        } else if (confirmPassword.get().isNullOrBlank().not() && newPassword.get().isNullOrBlank().not()) {
             if ((confirmPassword.get() == newPassword.get()).not()) {
                 confirmPasswordError.set(context.getString(R.string.confirm_password_error))
                 isValid = false
             }
         }
-
-        if (!TextUtils.isEmpty(currentPassword.get()) && (!TextUtils.isEmpty(newPassword.get()))) {
-            if (currentPassword.get() == newPassword.get()) {
-                newPasswordError.set(context.getString(R.string.same_password_error))
-                isValid = false
-            }
-        }
-
         return isValid
     }
 }
