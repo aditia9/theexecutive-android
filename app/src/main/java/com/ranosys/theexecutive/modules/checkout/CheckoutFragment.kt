0bcaa45fe8e5588ea179c5454cb041168c334c06
rashpal_singh25 <rashpal.singh@ranosys.com>
2018-05-07 17:55:55 +0530

    design checkout

---
+++ app/src/main/java/com/ranosys/theexecutive/modules/checkout/CheckoutFragment.kt
@@ -0,0 +1,9 @@
+package com.ranosys.theexecutive.modules.checkout
+
+import com.ranosys.theexecutive.base.BaseFragment
+
+class CheckoutFragment : BaseFragment(){
+
+    private lateinit var checkoutViewModel: CheckoutViewModel
+
+}
