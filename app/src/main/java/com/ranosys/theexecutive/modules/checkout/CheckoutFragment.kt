6055c50e1c12659cd9d1132329b3f6b62a1f1150
Nikhil Kumar Agarwal <nikhil.agarwal@ranosys.com>
2018-05-10 14:11:35 +0530

    Checkout UI in progress

--- app/src/main/java/com/ranosys/theexecutive/modules/checkout/CheckoutFragment.kt
+++ app/src/main/java/com/ranosys/theexecutive/modules/checkout/CheckoutFragment.kt
@@ -1,9 +1,26 @@
 package com.ranosys.theexecutive.modules.checkout
 
+import android.arch.lifecycle.ViewModelProviders
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ranosys.theexecutive.R
 import com.ranosys.theexecutive.base.BaseFragment
+import com.ranosys.theexecutive.databinding.FragmentCheckoutBinding
 
 class CheckoutFragment : BaseFragment(){
 
     private lateinit var checkoutViewModel: CheckoutViewModel
+    private lateinit var checkoutBinding: FragmentCheckoutBinding
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        checkoutBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_checkout, container, false)
+        checkoutViewModel = ViewModelProviders.of(this).get(CheckoutViewModel::class.java)
+
+        checkoutViewModel.getAddressApi()
+        checkoutViewModel.getCartItemsApi()
+        return checkoutBinding.root
+    }
 }
---
+++ app/src/main/java/com/ranosys/theexecutive/modules/checkout/CheckoutViewModel.kt
@@ -0,0 +1,19 @@
+package com.ranosys.theexecutive.modules.checkout
+
+import android.app.Application
+import com.ranosys.theexecutive.base.BaseViewModel
+
+/**
+ * @Details
+ * @Author Ranosys Technologies
+ * @Date 10-May-2018
+ */
+class CheckoutViewModel(application: Application): BaseViewModel(application) {
+    fun getAddressApi() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    fun getCartItemsApi() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
